73a8e1a8b4e66e706ed291df71348b17d30df506
JacktheGeat <jacklynch706@gmail.com>
2026-02-01 13:02:40 -0500

    updated inventory management

--- npc_base.gd
+++ npc_base.gd
@@ -13,7 +13,11 @@ var BoxBase = preload('res://dialog_box.tscn')
 @export var dialog_doneWithYou :Array[String]
 @export var dialog_stillWaiting :Array[String]
 
-@export var giftName :String
+@export var giftGiving : String
+@export var quantityGiven : int = 1
+
+@export var giftWanted : String
+@export var quantityWanted : int = 1
 
 var spokenYet := false
 var doneWithYou := false
@@ -31,15 +35,21 @@ func interact(playerInventory :Dictionary) -> DialogBox: #return the correct dia
 	elif doneWithYou: # if player inventory contains needed item
 		newDialog.assemble(myPicture, dialog_doneWithYou)
 
-	elif giftName == '':
-		playerInventory.set('Red Rose', 1)
-		doneWithYou = true
-		newDialog.assemble(myPicture, dialog_onGiftRecieved)
-
+	elif giftWanted == '': # recieving gift no conditions
+		playerInventory.set(giftGiving, playerInventory.get(giftGiving, 0)+1)
 		
-	elif playerInventory.get(giftName) and playerInventory[giftName] > 0: #recieving gift
-		playerInventory[giftName] -= 1
 		doneWithYou = true
+		
+		dialog_onGiftRecieved.append("You recieved a %s" % giftGiving)
+		newDialog.assemble(myPicture, dialog_onGiftRecieved)
+		
+	elif playerInventory.get(giftWanted) and playerInventory[giftWanted] > 0: #recieving gift with conditions
+		playerInventory[giftWanted] -= 1
+		playerInventory.set(giftGiving, playerInventory.get(giftGiving, 0)+1)
+		
+		doneWithYou = true
+		
+		dialog_onGiftRecieved.append("You recieved a [%s]" % giftGiving)
 		newDialog.assemble(myPicture, dialog_onGiftRecieved)
 
 	else: # waiting for gift
