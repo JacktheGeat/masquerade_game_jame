616be40568323ecac27f36428541990150c1bda3
JacktheGeat <jacklynch706@gmail.com>
2026-01-31 20:46:58 -0500

    player movement in box

--- Player.gd
+++ Player.gd
@@ -2,24 +2,29 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+const roomXMIN = 0
+const roomXMAX = 1152
+const roomYMIN = 0
+const roomYMAX = 648
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
+	var playerWidth = $PlayerSprite.get_rect().size.x * $PlayerSprite.scale.x /2
+	var playerHeight = $PlayerSprite.get_rect().size.y * $PlayerSprite.scale.y /2
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_pressed("ui_left") and position.x > roomXMIN + playerWidth:
+		velocity.x = -1 * SPEED
+	elif Input.is_action_pressed("ui_right") and position.x < roomXMAX - playerWidth:
+		velocity.x = 1 * SPEED
+	else: velocity.x = 0
+	if Input.is_action_pressed("ui_up") and position.y > roomYMIN + playerHeight:
+		velocity.y = -1 * SPEED
+	elif Input.is_action_pressed("ui_down") and position.y < roomYMAX - playerWidth:
+		velocity.y = 1 * SPEED
+	else: velocity.y = 0
 
 	move_and_slide()
