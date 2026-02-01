926b01cbfb79ae45a8cb3172d6c9289b6abff3b6
JacktheGeat <jacklynch706@gmail.com>
2026-01-31 22:18:13 -0500

    dialogue box works, no visuals yet

--- dialog_box.gd
+++ dialog_box.gd
@@ -10,11 +10,11 @@ var speechStrings :Array[String]
 func assemble(npcPicture, npcDialog, finalGift=null):
 	# install npc picture where it goes
 	# display first speech string
-	$Panel/portrait.texture = npcPicture
+	$HBoxContainer/VBoxContainer/Panel/portrait.texture = npcPicture
 
 	# if final gift is present, add "you got [item]" sting to end
 	pass
 
 func nextPage():
 	# if remaining pages, show next and return Wait indicator; else, return final gift or Done indicator
-	pass
+	return false
