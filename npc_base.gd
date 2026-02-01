e764bf6a34aa4cc29602a7ee688c9adce41ba4f6
HenryDevenport <devenpoh@union.edu>
2026-01-31 20:49:08 -0500

    ver1 of npcbase, dialogbox

--- npc_base.gd
+++ npc_base.gd
@@ -4,12 +4,31 @@ extends Node
 ## thing they want:
 ## thing they give:
 
+var BoxBase :PackedScene = preload('res://dialog_box.tscn')
 
 @export var myPicture :Texture2D
-@export var myDialog : Array[String]
+
+@export var dialog_initial :Array[String]
+@export var dialog_onGiftRecieved :Array[String]
+@export var dialog_doneWithYou :Array[String]
+
+@export var giftName :String
+@export var giftIcon :Texture2D
+
+var doneWithYou := false
 
 
-func getDialogue():
-	# assemble dialog box?
 
-	return myDialog
+func interact(playerInventory) -> DialogBox: #return the correct dialog
+
+	var newDialog = BoxBase.instantiate()
+	newDialog
+
+	if doneWithYou:
+		pass
+	elif (false): # if player inventory contains needed item
+		pass
+	else:
+		pass
+
+	return newDialog
